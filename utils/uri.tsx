f5bbb0ea493097a352a20d16c46c05789be791a4
Iqbal Farhan Nuruddin <iqbal.farhan931@gmail.com>
2024-08-20 22:33:25 +0700

    Update v3

--- utils/uri.tsx
+++ utils/uri.tsx
@@ -1,8 +1,2 @@
 export const SERVER_URI =
-  process.env.SERVER_URI || "http://192.168.1.5:8000/api/v1";
-
-export const MIDTRANS_URL = process.env.MIDTRANS_URL;
-
-export const MIDTRANS_CLIENT_KEY = process.env.MIDTRANS_CLIENT_KEY;
-
-export const MIDTRANS_SERVER_KEY = process.env.MIDTRANS_SERVER_KEY;
+  process.env.SERVER_URI || "http://192.168.1.14:8000/api/v1";
